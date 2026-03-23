9c3ad87eab3ab878081be782dcf46b00e1b9dfa1
pranav2004-ops <pranav2904kashyap@gmail.com>
2026-03-23 17:18:05 +0530

    github data added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
  require('dotenv').config()
  const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+const githubData=
+    {
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,7 +22,10 @@ app.get('/login', (req, res) => {
 app.get('/youtube',(req,res) =>{
     res.send('<h2>Chai aur code</h2>')
 })
+app.get('/github',(req,res) =>{
+    res.json(githubData)
+})
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
